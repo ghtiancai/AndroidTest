cfc3eac098f034636e188839bf1cb74e7251f992
ghtiancai <809027421@qq.com>
2016-12-20 15:07:41 +0800

    2

--- app/src/main/java/com/zhids/androidtest/MainActivity.java
+++ app/src/main/java/com/zhids/androidtest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("123");
+        System.out.print("123");
     }
 }
